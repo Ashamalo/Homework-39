db325b2777901d999510cae20f36348c19fa619e
Oleksandr Shamalo <shamalo1989@gmail.com>
2025-06-28 18:20:32 +0300

    fixed code

--- src/data/slides.ts
+++ src/data/slides.ts
@@ -1,19 +1,21 @@
 import type { Slide } from '../commonTypes/slide';
 
+const basePath = import.meta.env.BASE_URL;
+
 export const slidesData: Slide[] = [
   {
     id: 1,
-    imageUrl: "/images/nature1.jpg",
+    imageUrl: `${basePath}images/nature1.jpg`,
     caption: "Хмарочоси на узбережжі"
   },
   {
     id: 2,
-    imageUrl: "/images/nature2.jpg",
+    imageUrl: `${basePath}images/nature2.jpg`,
     caption: "Рідкісний вид орла"
   },
   {
     id: 3,
-    imageUrl: "/images/nature3.jpg",
+    imageUrl: `${basePath}images/nature3.jpg`,
     caption: "Скелясте узбережжя з неспокійною водою"
   }
 ];
